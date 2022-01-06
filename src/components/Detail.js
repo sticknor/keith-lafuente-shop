a212bf8fd90c8cfafbfb9ed1e820168c4b5c1e7f
Sam Ticknor <srticknor@gmail.com>
2022-01-06 16:16:14 -0500

    Added variants to the details

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -20,14 +20,26 @@ function Detail(props) {
 
   const [imageIndex, setImageIndex] = useState(0);
   const [product, setProduct] = useState(null);
+  const [variantIndex, setVariantIndex] = useState(0);
   const [prevEnabled, setPrevEnabled] = useState(false);
   const [nextEnabled, setNextEnabled] = useState(false);
+  const [addToBagEnabled, setAddToBagEnabled] = useState(true);
 
   useEffect(() => {
     for (var _product of products) {
       if (_product.handle === id) setProduct(_product);
     }
   }, [id, products]);
+
+  useEffect(() => {
+    if (product != null && product.variants.length > variantIndex) {
+      if (product.variants[variantIndex].available === false) {
+        setAddToBagEnabled(false);
+      } else {
+        setAddToBagEnabled(true);
+      }
+    }
+  }, [product, variantIndex]);
 
   useEffect(() => {
     if (product != null && product.images !== undefined) {
@@ -56,6 +68,10 @@ function Detail(props) {
     }
   };
 
+  const handleChangeVariant = (event) => {
+    setVariantIndex(event.target.value);
+  }
+
   const handlePrevClick = () => {
     setImageIndex(imageIndex - 1);
     setPrevEnabled(imageIndex - 1 >= 0);
@@ -79,8 +95,8 @@ function Detail(props) {
           <Card
             title={product.title}
             image={product.images[imageIndex]}
-            price={product.variants[0].price}
-            sold={false}
+            price={product.variants[variantIndex].price}
+            sold={!product.variants[variantIndex].available}
           />
         </div>
         {prevEnabled && (
@@ -97,11 +113,26 @@ function Detail(props) {
           {product.title}
         </div>
         <div className="detail-section">{product.description}</div>
-        <div className="detail-section">${product.variants[0].price}</div>
+        {product.variants.length <= 1 &&
+          <div className="detail-section">${product.variants[variantIndex].price}</div>
+        }
+        {product.variants.length > 1 &&
+          <div className="detail-variants">
+            <select value={variantIndex} onChange={handleChangeVariant}>
+              {product.variants.map((variant, index) => {
+                if (variant.available) {
+                  return <option value={index} key={index}>{variant.title} - ${variant.price}</option>
+                } else {
+                  return <option disabled value={index} key={index}>{variant.title} (sold out)</option>
+                }
+              })}
+            </select>
+          </div>
+        }
         <div
-          className="add-to-bag-button detail-section"
+          className={addToBagEnabled ? "add-to-bag-button detail-section" : "add-to-bag-button-disabled detail-section"}
           onClick={() => {
-            addToCart(product.variants[0].id);
+            addToCart(product.variants[variantIndex].id);
           }}
         >
           {' '}
@@ -109,27 +140,6 @@ function Detail(props) {
         </div>
       </div>
     </>
-    // <div style={{ width: '25vw', minWidth: '300px', position: 'relative', margin: '24px', transform: `rotate(${rotation}deg)` }}>
-    //     <div className={"card-container"}>
-    //         { /* Buy now button */ }
-    //         {/* <div
-    //             style={{ cursor: 'pointer', fontWeight: 'bold' }}
-    //             onClick={() => { addToCart(product.variants[0].id)}}
-    //         >
-    //             ADD TO CART
-    //         </div> */}
-    //         <img className={"card-image-underlay"} alt={title} style={{ width: '81%', left: '7%' }} src={images[imageIndex].src} />
-    //         <img className={"card-graphic-overlay"} src={CardDefault} />
-    //         <div className={"card-text-overlay"}>
-    //             <div className={"card-title"}>{title}</div>
-    //             {sold
-    //               ? <div className={"card-price card-sold-out"}>{'Sold\nout'}</div>
-    //               : <div className={"card-price"}>{`$${price.split('.')[0]}`}</div>
-    //             }
-    //         </div>
-
-    //     </div>
-    // </div>
   );
 }
 
