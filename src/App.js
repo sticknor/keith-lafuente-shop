18a7b4e66264ad2a26d03252296163bda5249855
Sam Ticknor <srticknor@gmail.com>
2022-01-28 09:14:07 -0500

    http to https

--- src/App.js
+++ src/App.js
@@ -206,7 +206,7 @@ export default function App() {
     <>
       <Helmet>
         <title>Keith Lafuente</title>
-        <link rel="icon" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`} sizes="16x16" />
+        <link rel="icon" href={`data:image/svg+xml,<svg xmlns=%22https://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`} sizes="16x16" />
       </Helmet>
       <GlobalStyles globalCss={globalCss} />
       <HashRouter basename='/'>
