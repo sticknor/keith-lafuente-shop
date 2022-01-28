76be42288b01790ff80fec7ce86109a7dd2f2166
Sam <srticknor@gmail.com>
2022-01-28 10:34:18 -0500

    Update Stocklists.js

--- src/components/Stocklists.js
+++ src/components/Stocklists.js
@@ -8,7 +8,7 @@ function Stocklists() {
                     Stocklists
                 </div>
                 <div>
-                    <a style="color:var(--stocklists-link-color);" href='https://wayawaya.co/' target="_blank" rel='noreferrer noopener'>WAYAWAYA</a>
+                    <a style={{ color: var(--stocklists-link-color); }} href='https://wayawaya.co/' target="_blank" rel='noreferrer noopener'>WAYAWAYA</a>
                 </div>
             </div>
         </div>
