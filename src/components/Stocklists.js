4911b38e1283dc80104dd56cc3507bfb0b4c1476
Sam <srticknor@gmail.com>
2022-01-28 10:12:45 -0500

    Add files via upload

---
+++ src/components/Stocklists.js
@@ -0,0 +1,18 @@
+// React
+import React from 'react';
+function Stocklists() {
+    return (
+        <div className="template-container">
+            <div className="template-content">
+                <div className="template-title">
+                    Stocklists
+                </div>
+                <div>
+                    <a href='https://wayawaya.co/' target="_blank">WAYAWAYA</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Stocklists;
