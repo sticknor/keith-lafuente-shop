85b14bc011cd3fa4166d08b4d42b87073f0e7948
Sam <srticknor@gmail.com>
2022-01-28 09:57:01 -0500

    Add files via upload

---
+++ src/components/Stocklists.js
@@ -0,0 +1,21 @@
+// React
+import React from 'react';
+
+function Stocklists() {
+    return (
+        <div className="about-container">
+            <div className="about-content">
+                <div className="about-title">
+                    Stocklists
+                </div>
+                <div>
+                    <a href="">Wawawaw</a>
+					<img src="">
+					<div style="width:50px;height:50px;background-color: red;"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Stocklists;
